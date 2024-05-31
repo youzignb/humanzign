932470839fbbecc3a653b4c9b7813b892435421f
youzignb <91051290+youzignb@users.noreply.github.com>
2024-05-31 23:43:29 +0200

    removed title

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function shareImage() {
           columnValues: {
                  "Name": “userID”,
         "Ca9jF": “5”, // AppID
-        "6xrkR": "Dezygn Native“, // Title
+        "6xrkR": "DezygnNative“, 
         "TZdW5": “Image”, // Datatype
         "7z50p": "imageSrc", // ImageURL
         "mlPS8": “true”, //private
